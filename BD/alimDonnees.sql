f956836965b6f90dce9b6c0b50872ee7386f0959
Clement97 <Clement-aldobrandi@hotmail.fr>
2017-03-01 17:43:58 +0100

    creStru + supStru + alimDo OK

--- BD/alimDonnees.sql
+++ BD/alimDonnees.sql
@@ -8,24 +8,32 @@ insert into Animal(idClient,type,nom,identification,carnetVaccinationValide,date
 insert into Animal(idClient,type,nom,identification,carnetVaccinationValide,dateUpload) values (3,'chien','luna','9932XL23',1,'2015-01-22');
 insert into Animal(idClient,type,nom,identification,carnetVaccinationValide,dateUpload) values (3,'rongeur','lerongeur','230MSAZ0214',1,'2015-06-01');
 
--- insert into Reservation(idAnimal,idClient,dateReservation,dateDebut,dateFin,montant,valide) values (1,1,'2016-05-19','2016-06-21','2016-06-25',);
--- insert into Reservation(idAnimal,idClient,dateReservation,dateDebut,dateFin,montant,valide) values (2,2,'2016-04-26','2016-05-21','2016-05-29',);
--- insert into Reservation(idAnimal,idClient,dateReservation,dateDebut,dateFin,montant,valide) values (3,1,'2016-03-24','2016-02-11','2016-03-25',);
--- insert into Reservation(idAnimal,idClient,dateReservation,dateDebut,dateFin,montant,valide) values (4,3,'2016-02-20','2016-02-21','2016-06-28',);
--- insert into Reservation(idAnimal,idClient,dateReservation,dateDebut,dateFin,montant,valide) values (5,4,'2016-01-09','2016-01-21','2016-02-12',);
--- insert into Reservation(idAnimal,idClient,dateReservation,dateDebut,dateFin,montant,valide) values (1,1,'2016-11-11','2016-12-11','2016-12-06',);
--- insert into Reservation(idAnimal,idClient,dateReservation,dateDebut,dateFin,montant,valide) values (2,2,'2016-01-01','2016-01-07','2016-01-09',);
--- insert into Reservation(idAnimal,idClient,dateReservation,dateDebut,dateFin,montant,valide) values (3,1,'2016-05-31','2016-06-05','2016-06-25',);
+insert into Reservation(idAnimal,idClient,dateReservation,dateDebut,dateFin,montant,valide) values (1,1,'2016-05-19','2016-06-21','2016-06-25',125,1);
+insert into Reservation(idAnimal,idClient,dateReservation,dateDebut,dateFin,montant,valide) values (2,2,'2016-04-26','2016-05-21','2016-05-29',270,1);
+insert into Reservation(idAnimal,idClient,dateReservation,dateDebut,dateFin,montant,valide) values (3,1,'2016-03-24','2016-02-11','2016-03-25',1505,1);
+insert into Reservation(idAnimal,idClient,dateReservation,dateDebut,dateFin,montant,valide) values (4,3,'2016-02-20','2016-02-21','2016-02-28',280,1);
+insert into Reservation(idAnimal,idClient,dateReservation,dateDebut,dateFin,montant,valide) values (5,3,'2016-01-09','2016-01-21','2016-02-12',220,1);
+insert into Reservation(idAnimal,idClient,dateReservation,dateDebut,dateFin,montant,valide) values (1,1,'2016-11-11','2016-12-06','2016-12-11',180,1);
+insert into Reservation(idAnimal,idClient,dateReservation,dateDebut,dateFin,montant,valide) values (2,2,'2016-01-01','2016-01-07','2016-01-09',75,1);
+insert into Reservation(idAnimal,idClient,dateReservation,dateDebut,dateFin,montant,valide) values (3,1,'2016-05-31','2016-06-05','2016-06-25',800,1);
 
 insert into Operateur(login,password,nom,prenom) values('log1','mdp1','nom1','prenom1');
 insert into Administrateur(login,password,nom,prenom) values('log1','mdp1','nom1','prenom1');
 
--- client réservé en 2016, plus précisément en janvier 2016
--- Clients ont réservé + de 8j (en plusieurs réservation)
--- Faire plusieurs animaux
-
--- -Client qui a réservé le plus de jours en 2016
--- -Nbr Client qui ont réservé + de 8j au total en 2016
--- -Capacité totale de la pension par animal au jour d’aujourd’hui.
 
 
+/*
+
+client réservé en 2016, plus précisément en janvier 2016
+Clients ont réservé + de 8j (en plusieurs réservation)
+Faire plusieurs animaux
+
+-Client qui a réservé le plus de jours en 2016
+-Nbr Client qui ont réservé + de 8j au total en 2016
+-Capacité totale de la pension par animal au jour d’aujourd’hui.
+
+
+Le tarif d’une place isolée est de 30€/jour pour un chat, 40€/jour pour un chien et 15€/jour pour un rongeur.
+Le tarif d’une place partagée est de 25€/jour pour un chat, 35€/jour pour un chien et 10€/jour pour un rongeur.
+Un supplément de 5€/jour est demandé si le maître souhaite pour son chien une promenade journalière en laisse dans un parc.
+*/
---
+++ BD/test.sql
@@ -0,0 +1,217 @@
+drop table Operateur, Reservation, Animal, Administrateur;
+drop table Client;
+
+-- phpMyAdmin SQL Dump
+-- version 4.6.4
+-- https://www.phpmyadmin.net/
+--
+-- Client :  localhost:8889
+-- Généré le :  Lun 20 Février 2017 à 15:59
+-- Version du serveur :  5.6.28
+-- Version de PHP :  7.0.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+--
+-- Base de données :  `PDS`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Administrateur`
+--
+
+CREATE TABLE `Administrateur` (
+  `idAdministrateur` int(11) NOT NULL,
+  `login` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `prenom` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Animal`
+--
+
+CREATE TABLE `Animal` (
+  `idAnimal` int(11) NOT NULL,
+  `idClient` int(11) NOT NULL,
+  `type` varchar(255) NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `identification` varchar(255) NOT NULL,
+  `carnetVaccinationValide` tinyint(1) NOT NULL,
+  `dateUpload` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Client`
+--
+
+CREATE TABLE `Client` (
+  `idClient` int(11) NOT NULL,
+  `login` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `prenom` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `tel` varchar(12) NOT NULL,
+  `adresse` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Operateur`
+--
+
+CREATE TABLE `Operateur` (
+  `idOperateur` int(11) NOT NULL,
+  `login` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `nom` varchar(255) NOT NULL,
+  `prenom` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `Reservation`
+--
+
+CREATE TABLE `Reservation` (
+  `idReservation` int(11) NOT NULL,
+  `idAnimal` int(11) NOT NULL,
+  `idClient` int(11) NOT NULL,
+  `dateReservation` date NOT NULL,
+  `dateDebut` date NOT NULL,
+  `dateFin` date NOT NULL,
+  `montant` int(11) NOT NULL,
+  `valide` tinyint(1) NOT NULL
+
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `Administrateur`
+--
+ALTER TABLE `Administrateur`
+  ADD PRIMARY KEY (`idAdministrateur`);
+
+--
+-- Index pour la table `Animal`
+--
+ALTER TABLE `Animal`
+  ADD PRIMARY KEY (`idAnimal`);
+
+--
+-- Index pour la table `Client`
+--
+ALTER TABLE `Client`
+  ADD PRIMARY KEY (`idClient`);
+
+--
+-- Index pour la table `Operateur`
+--
+ALTER TABLE `Operateur`
+  ADD PRIMARY KEY (`idOperateur`);
+
+--
+-- Index pour la table `Reservation`
+--
+ALTER TABLE `Reservation`
+  ADD PRIMARY KEY (`idReservation`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `Administrateur`
+--
+ALTER TABLE `Administrateur`
+  MODIFY `idAdministrateur` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `Animal`
+--
+ALTER TABLE `Animal`
+  MODIFY `idAnimal` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `Client`
+--
+ALTER TABLE `Client`
+  MODIFY `idClient` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `Operateur`
+--
+ALTER TABLE `Operateur`
+  MODIFY `idOperateur` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `Reservation`
+--
+ALTER TABLE `Reservation`
+  MODIFY `idReservation` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Contraintes pour les tables exportées
+--
+
+--
+-- Contraintes pour la table `Animal`
+--
+ALTER TABLE `Animal`
+  ADD CONSTRAINT `fk_animal_client` FOREIGN KEY (`idClient`) REFERENCES `Client` (`idClient`);
+
+--
+-- Contraintes pour la table `Reservation`
+--
+ALTER TABLE `Reservation`
+  ADD CONSTRAINT `fk_reservation_animal` FOREIGN KEY (`idAnimal`) REFERENCES `Animal` (`idAnimal`),
+  ADD CONSTRAINT `fk_reservation_client` FOREIGN KEY (`idClient`) REFERENCES `Client` (`idClient`);
+
+insert into Client(login,password,nom,prenom,email,tel,adresse) values ('log1','mdp1','nom1','prenom1','email@hotmail.fr','0143863123','7 rue des potiers');
+insert into Client(login,password,nom,prenom,email,tel,adresse) values ('log2','mdp2','nom2','prenom2','email@hotmail.fr','0143970231','9 rue des potiers');
+insert into Client(login,password,nom,prenom,email,tel,adresse) values ('log3','mdp3','nom3','prenom3','email@hotmail.fr','0143120571','8 rue des potiers');
+
+insert into Animal(idClient,type,nom,identification,carnetVaccinationValide,dateUpload) values (1,'chat','tigrou','9423XL219',1,'2015-02-11');
+insert into Animal(idClient,type,nom,identification,carnetVaccinationValide,dateUpload) values (2,'chat','edgar','0321IEZA231',1,'2015-03-11');
+insert into Animal(idClient,type,nom,identification,carnetVaccinationValide,dateUpload) values (1,'chien','jean-pierre','0231LZAE7523',1,'2015-06-01');
+insert into Animal(idClient,type,nom,identification,carnetVaccinationValide,dateUpload) values (3,'chien','luna','9932XL23',1,'2015-01-22');
+insert into Animal(idClient,type,nom,identification,carnetVaccinationValide,dateUpload) values (3,'rongeur','lerongeur','230MSAZ0214',1,'2015-06-01');
+
+insert into Reservation(idAnimal,idClient,dateReservation,dateDebut,dateFin,montant,valide) values (1,1,'2016-05-19','2016-06-21','2016-06-25',125,1);
+insert into Reservation(idAnimal,idClient,dateReservation,dateDebut,dateFin,montant,valide) values (2,2,'2016-04-26','2016-05-21','2016-05-29',270,1);
+insert into Reservation(idAnimal,idClient,dateReservation,dateDebut,dateFin,montant,valide) values (3,1,'2016-03-24','2016-02-11','2016-03-25',1505,1);
+insert into Reservation(idAnimal,idClient,dateReservation,dateDebut,dateFin,montant,valide) values (4,3,'2016-02-20','2016-02-21','2016-02-28',280,1);
+insert into Reservation(idAnimal,idClient,dateReservation,dateDebut,dateFin,montant,valide) values (5,3,'2016-01-09','2016-01-21','2016-02-12',220,1);
+insert into Reservation(idAnimal,idClient,dateReservation,dateDebut,dateFin,montant,valide) values (1,1,'2016-11-11','2016-12-06','2016-12-11',180,1);
+insert into Reservation(idAnimal,idClient,dateReservation,dateDebut,dateFin,montant,valide) values (2,2,'2016-01-01','2016-01-07','2016-01-09',75,1);
+insert into Reservation(idAnimal,idClient,dateReservation,dateDebut,dateFin,montant,valide) values (3,1,'2016-05-31','2016-06-05','2016-06-25',800,1);
+
+insert into Operateur(login,password,nom,prenom) values('log1','mdp1','nom1','prenom1');
+insert into Administrateur(login,password,nom,prenom) values('log1','mdp1','nom1','prenom1');
+
+
+
+/*
+
+client réservé en 2016, plus précisément en janvier 2016
+Clients ont réservé + de 8j (en plusieurs réservation)
+Faire plusieurs animaux
+
+-Client qui a réservé le plus de jours en 2016
+-Nbr Client qui ont réservé + de 8j au total en 2016
+-Capacité totale de la pension par animal au jour d’aujourd’hui.
+
+
+Le tarif d’une place isolée est de 30€/jour pour un chat, 40€/jour pour un chien et 15€/jour pour un rongeur.
+Le tarif d’une place partagée est de 25€/jour pour un chat, 35€/jour pour un chien et 10€/jour pour un rongeur.
+Un supplément de 5€/jour est demandé si le maître souhaite pour son chien une promenade journalière en laisse dans un parc.
+*/
